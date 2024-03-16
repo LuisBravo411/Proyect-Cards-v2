e72a960552640b2b3d465c5303345e5d3b972948
Luis Bravo <luiscarlosbravo411@gmail.com>
2024-03-16 08:04:07 -0400

    Añadiendo Card #1 Color Fleeper y cosas generales

--- app/app.js
+++ app/app.js
@@ -16,27 +16,88 @@ sidebarClose.addEventListener("click", function(){
 
 // CARDS
 
-// const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card");
+const cardOverlay = document.querySelector(".card-overlay")
 
-// cards.forEach(function(card){
-//     card.addEventListener("click",function(e){
-//         // console.log(card)
-//         // let cardModal = function(){
+cardOverlay.addEventListener("click",function(){
+    cardOverlay.classList.add("none")
+    // cards.classList.remove("show-card")
+})
 
-//         // }
-//         const modalCard = e.currentTarget.classList;
-//         if (modalCard.contains("card-1")){
-//             // console.log("hello card-1")
-//             const card1 = document.querySelector(".card-1");
-//             card1.addEventListener("click",function(){
-//                 card1.classList.toggle("card-modal")
-//             })
-//         } if (modalCard.contains("card-2")){
-//             console.log("hello card-2")
-//         } if (modalCard.contains("card-3")){
-//             console.log("hello card-3")
-//         } if (modalCard.contains("card-4")){
-//             console.log("hello card-4")
-//         }
-//     })
-// })
+cards.forEach(function(card){
+    card.addEventListener("click",function(e){
+        // console.log(card)
+        let openOverlay = function(){
+            cardOverlay.classList.remove("none")
+        }
+        let showCard = function(e){
+            e.classList.add("show-card");
+            openOverlay()
+        }
+        let cardVisibility = function(e){
+            // const card1 = document.querySelector(".card-1");
+            e.addEventListener("click", showCard(e))
+            cardOverlay.addEventListener("click", function(){
+                e.classList.remove("show-card")
+            })
+        }
+        
+        
+        const modalCard = e.currentTarget.classList;
+        // Card #1
+        if (modalCard.contains("card-1")){
+            const card1 = document.querySelector(".card-1");
+            cardVisibility(card1)
+            // Card Code CARD #1-----------------------------------------------------------
+            const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+            const btnCF = document.getElementById("btnCF");
+            const color = document.querySelector(".color");
+            const colorFleeper = document.querySelector(".color-fleeper");
+
+            function randomColor(){
+                let hexColor = "#";
+                
+                for (let i = 0; i < 6; i++){
+                    hexColor += hex[getRandomNumber()]
+                }
+                
+                color.textContent = hexColor;
+                color.style.color = hexColor;
+                colorFleeper.style.backgroundColor = hexColor;
+                
+            }
+            
+            btnCF.addEventListener("click", randomColor);
+            
+            function getRandomNumber(){
+                return Math.floor(Math.random()* hex.length);
+            }
+            
+            // End code------------------------------------------------------------
+        }
+        // Card #2 
+        if (modalCard.contains("card-2")){
+            
+            const card2 = document.querySelector(".card-2");
+            cardVisibility(card2)
+
+
+        } 
+        // Card #3
+        if (modalCard.contains("card-3")){
+ 
+            const card3 = document.querySelector(".card-3");
+            cardVisibility(card3)
+
+
+        } 
+        // Card #4
+        if (modalCard.contains("card-4")){
+
+            const card4 = document.querySelector(".card-4");
+            cardVisibility(card4 )
+
+
+        }
+    })
+})
